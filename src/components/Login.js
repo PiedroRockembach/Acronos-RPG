23c6e2b2c948f3505f3c9f0fdd6859135d136c19
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-11-29 20:06:16 -0300

    arruma o forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,12 @@ class Login extends React.Component {
   }
   sender = async (evt) => {
     evt.preventDefault();
-    console.log(evt.target);
     await fetch(evt.target.action, {
       method: 'POST',
       body: new FormData(evt.target)
-    }).then((toPost) => toPost.json())
+    }).then((toPost) => toPost.json()).then(() => {
+      evt.target.childNodes[2].innerHTML = 'Enviado!!!!!!!!!!!!!!!';
+    })
     
   }
   render() {
@@ -18,6 +19,7 @@ class Login extends React.Component {
       <form action='https://sheetdb.io/api/v1/gai9m9j5zi8qb' method="POST" id="sheetdb-form" onSubmit={this.sender}>
         <input type="text" id="user-input" placeholder="Nome" name="data[name]"/>
         <input type="text" name="data[text]" id="user-password" placeholder="Me diga algo." />
+
         <button type="submit">Enviar</button>
       </form>
     );
