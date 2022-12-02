17225f44ceb8bc1ea9858daabfcb557855553093
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-02 08:48:36 -0300

    cria api

---
+++ src/data/dataBase.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { google } = require('googleapis')
+
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+// app.use('/')
+app.get("/product", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "get data successfuly"
+    });
+  } catch (error) {
+    console.error(error);
+    return resizeBy.status(500).send("Server error");
+  }
+})
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
