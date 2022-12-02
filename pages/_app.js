2a2c22edf0d3deabbbf35faef1055d8589b53d7b
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-02 08:10:18 -0300

    inicia teste express

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Login from '../src/components/Login';
-// import AsideMenu from './src/components/asideMenu';
+import AsideMenu from '../src/components/AsideMenu';
 import Header from '../src/components/Header';
 import Perfil from '../src/components/Perfil';
 import '../src/css/secundary-style.css'
 import '../src/css/style.css'
 
 class App extends React.Component {
+
   render() {
     return(
       <div>
         <Header />
         <div className='body'>
+        <AsideMenu />
           <main>
             <div className='perfil'>
               <Perfil />
---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Login extends React.Component {
+
+  render() {
+    
+    return (
+     <>
+        <input></input>
+        <input></input>
+        <button >Enviar</button>
+        <button >mostrar itens</button>
+      
+     </>
+    );
+  }
+}
+ 
+export default Login;
